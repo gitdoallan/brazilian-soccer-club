315848191dd376c87e129d055456db83158f878d
gitdoallan <mailtoallan@icloud.com>
2022-08-18 19:18:52 -0300

    feat: validationLogin middleware implemented

--- app/backend/src/middlewares/validateLogin.middleware.ts
+++ app/backend/src/middlewares/validateLogin.middleware.ts
@@ -1,11 +1,16 @@
 import { Request, Response, NextFunction } from 'express';
 import { ErrorHandler } from '../utils/ErrorHandler';
+import { EMAIL_REGEX } from '../utils/regex';
 
-export const validateLoginMiddleware = (req: Request, _res: Response, next: NextFunction): void => {
+export const validateLoginMiddleware = (req: Request, res: Response, next: NextFunction): void => {
   const { email, password } = req.body;
 
   if (!email || !password) {
     return next(new ErrorHandler(400, 'All fields must be filled'));
+  }
+
+  if (!EMAIL_REGEX.test(email) || password.length < 6) {
+    return next(new ErrorHandler(401, 'Incorrect email or password'));
   }
 
   next();
