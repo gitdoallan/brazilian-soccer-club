ea798ea66f5fbfc9610689bdd1a1339df915a6f3
gitdoallan <mailtoallan@icloud.com>
2022-08-18 10:54:31 -0300

    feat: adding loginRepository to login route

--- app/backend/src/routes/login.route.ts
+++ app/backend/src/routes/login.route.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import Repository from '../repository/login.repository';
 import Controller from '../controllers/login.controller';
 import Service from '../services/login.service';
 
-const loginService = new Service();
+const loginRepository = new Repository();
+const loginService = new Service(loginRepository);
 const loginController = new Controller(loginService);
 
 const router = Router();
