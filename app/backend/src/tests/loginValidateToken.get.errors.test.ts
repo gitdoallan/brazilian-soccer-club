666587ac75a11947ca8fab928a5934d52edc6fd9
gitdoallan <mailtoallan@icloud.com>
2022-08-18 23:26:43 -0300

    test: validateToken errors

---
+++ app/backend/src/tests/loginValidateToken.get.errors.test.ts
@@ -0,0 +1,40 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { STATUS_INTERNAL_SERVER_ERROR, STATUS_NOT_FOUND } from '../utils/httpStatus';
+import { INVALID_TOKEN } from '../utils/mocks';
+
+import { app } from '../app';
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Endpoint /login/validate FAILED requisition', () => {
+  let chaiHttpResponse: Response;
+
+  it('Token is not found', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .get('/login/validate')
+      .send();
+
+    const { status } = chaiHttpResponse;
+    expect(status).to.be.equal(STATUS_NOT_FOUND);
+  });
+
+  it('Token is invalid', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .get('/login/validate')
+      .set('Authorization', INVALID_TOKEN)
+      .send();
+
+    const { status } = chaiHttpResponse;
+    expect(status).to.be.equal(STATUS_INTERNAL_SERVER_ERROR);
+  });
+});
