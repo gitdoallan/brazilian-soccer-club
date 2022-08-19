1304f57124dcb13eb85f503cd9800025059f481b
gitdoallan <mailtoallan@icloud.com>
2022-08-19 05:51:31 -0300

    test: /matches listing all matches

---
+++ app/backend/src/tests/matches.get.success.test.ts
@@ -0,0 +1,43 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import Matches from '../database/models/Matches';
+
+import { STATUS_SUCCESS } from '../utils/httpStatus';
+import { MOCK_MATCHES } from '../utils/mocks';
+
+import { app } from '../app';
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Endpoint /matches SUCCESSFUL requisition', () => {
+  let chaiHttpResponse: Response;
+
+  beforeEach(async () => {
+    sinon
+      .stub(Matches, "findAll")
+      .resolves(MOCK_MATCHES as any);
+  });
+
+  afterEach(()=>{
+    (Matches.findAll as sinon.SinonStub).restore();
+  })
+
+  it('Returns an array of matches', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .get('/matches')
+      .send();
+
+    const result = chaiHttpResponse.body;
+    const { status } = chaiHttpResponse;
+    expect(result).to.deep.equals(MOCK_MATCHES);
+    expect(status).to.equals(STATUS_SUCCESS);
+  });
+});
--- app/backend/src/utils/mocks.ts
+++ app/backend/src/utils/mocks.ts
@@ -24,60 +24,35 @@ export const MOCK_TEAMS = [
     id: 2,
     teamName: 'Bahia',
   },
+];
+
+export const MOCK_MATCHES = [
   {
-    id: 3,
-    teamName: 'Botafogo',
+    id: 1,
+    homeTeam: 16,
+    homeTeamGoals: 1,
+    awayTeam: 8,
+    awayTeamGoals: 1,
+    inProgress: false,
+    teamHome: {
+      teamName: 'São Paulo',
+    },
+    teamAway: {
+      teamName: 'Grêmio',
+    },
   },
   {
-    id: 4,
-    teamName: 'Corinthians',
-  },
-  {
-    id: 5,
-    teamName: 'Cruzeiro',
-  },
-  {
-    id: 6,
-    teamName: 'Ferroviária',
-  },
-  {
-    id: 7,
-    teamName: 'Flamengo',
-  },
-  {
-    id: 8,
-    teamName: 'Grêmio',
-  },
-  {
-    id: 9,
-    teamName: 'Internacional',
-  },
-  {
-    id: 10,
-    teamName: 'Minas Brasília',
-  },
-  {
-    id: 11,
-    teamName: 'Napoli-SC',
-  },
-  {
-    id: 12,
-    teamName: 'Palmeiras',
-  },
-  {
-    id: 13,
-    teamName: 'Real Brasília',
-  },
-  {
-    id: 14,
-    teamName: 'Santos',
-  },
-  {
-    id: 15,
-    teamName: 'São José-SP',
-  },
-  {
-    id: 16,
-    teamName: 'São Paulo',
+    id: 2,
+    homeTeam: 9,
+    homeTeamGoals: 1,
+    awayTeam: 14,
+    awayTeamGoals: 1,
+    inProgress: false,
+    teamHome: {
+      teamName: 'Internacional',
+    },
+    teamAway: {
+      teamName: 'Santos',
+    },
   },
 ];
